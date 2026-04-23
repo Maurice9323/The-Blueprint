b1aec7e3aa4f20a9f804e4b00477a9957452a2a0
Maurice9323 <3221sjmo@gmail.com>
2026-04-23 14:59:47 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'blueprint-v15-cache';
+const urlsToCache = [
+  'style-guide-v15.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
